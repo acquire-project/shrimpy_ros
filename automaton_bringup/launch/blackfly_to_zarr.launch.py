4f206e40596421fcbeb381a9cf7e8eb627821107
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-10-02 10:47:12 -0700

    initial automaton_bringup package. Launch file simply includes spinnaker driver and sets it up with a minimal set of necessary parameters.

---
+++ automaton_bringup/launch/blackfly_to_zarr.launch.py
@@ -0,0 +1,27 @@
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('spinnaker_camera_driver'),
+                    'launch',
+                    'driver_node.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'serial': "'21278186'",
+                'camera_type': 'blackfly_s',
+                'camera_name': 'blackfly'
+            }.items()
+        )
+    ])
+    

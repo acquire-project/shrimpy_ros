d801955b613b91fe576fa56b7498640554dbd880
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-11-06 08:04:55 -0800

    added SetAnalogRange Service

--- triggerscope_ros/triggerscope_ros/triggerscope_server_node.py
+++ triggerscope_ros/triggerscope_ros/triggerscope_server_node.py
@@ -2,23 +2,47 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-from triggerscope_msgs.srv import SetDigitalOut
+import triggerscope_msgs.srv as srv 
 import serial
 
+# Map of voltage ranges to their corresponding min and max values
+# This is used to convert the range enum to the actual voltage range
+# that the Triggerscope expects
+# These are constants, do not modify
+ANALOG_VOLTAGE_RANGES = {
+    srv.SetAnalogRange_Request.VOLTAGE_RANGE_0_TO_5: (0., 5.),
+    srv.SetAnalogRange_Request.VOLTAGE_RANGE_0_TO_10: (0., 10.),
+    srv.SetAnalogRange_Request.VOLTAGE_RANGE_NEGATIVE_5_TO_5: (-5., 5.),
+    srv.SetAnalogRange_Request.VOLTAGE_RANGE_NEGATIVE_10_TO_10: (-10., 10.),
+    srv.SetAnalogRange_Request.VOLTAGE_RANGE_NEGATIVE_2_TO_2: (-2., 2.),
+}
 
 class TriggerscopeServerNode(Node):
-
+    """
+    Node that interfaces a Triggerscope device with ROS2
+    """
+    
     def __init__(self):
+        """
+        Initialize the TriggerscopeServerNode
+        """
+        
+        # Map of analog voltage ranges keyed by channel
+        self.analog_ranges = {}
+        
+        # Initialize the Base Node Class
         super().__init__('triggerscopeserver')
 
-        self.srv = self.create_service(SetDigitalOut, 'set_digital_out', self.set_digital_output_callback)
+        # Create services
+        self.srv = self.create_service(srv.SetDigitalOut, 'set_digital_out', self.set_digital_output_callback)
+        self.srv = self.create_service(srv.SetAnalogRange, 'set_analog_range', self.set_analog_range_callback)
 
         # Initialize serial port
         self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=0.2)
         self.get_logger().info('TriggerscopeServer node initialized')
 
 
-    def set_digital_output_callback(self, request, response):
+    def set_digital_output_callback(self, request:srv.SetAnalogOut_Request, response:srv.SetAnalogOut_Response):
         self.get_logger().debug('Received request to set digital output %d to %d' % (request.channel, request.state))
         outstring = f"SDO{int(request.channel)}-{int(request.state)}"
         self.serial_port.write((outstring + '\n').encode("ascii"))
@@ -30,19 +54,40 @@ class TriggerscopeServerNode(Node):
             response.success = True
         else:
             self.get_logger().error('Failed to set digital output %d to %d' % (request.channel, request.state))
-            self.get_logger().error('Received: \'%s\'' % line)
+            self.get_logger().error('Received: %s' % line)
             response.success = False
             
         return response
 
-    def timer_callback(self):
-        self.read_from_serial()
+    def set_analog_range_callback(self, request:srv.SetAnalogOut_Request, response:srv.SetAnalogOut_Response):
+        self.get_logger().debug('Received request to set analog range %d to %d' % (request.channel, request.range))
+        outstring = f"SAR{int(request.channel)}-{int(request.range)}"
+        self.serial_port.write((outstring + '\n').encode("ascii"))
+        
+        line = self.serial_port.readline().strip().decode('ascii')
+
+        if line and (line == '!' + outstring):
+            self.get_logger().debug('Successfully set analog range %d to %d' % (request.channel, request.range))
+            response.success = True
+        else:
+            self.get_logger().error('Failed to set analog range %d to %d' % (request.channel, request.range))
+            self.get_logger().error('Received: %s' % line)
+            response.success = False
+        
+        if response.success:
+            self.analog_ranges[request.channel] = ANALOG_VOLTAGE_RANGES[request.range]
+            
+        return response
+        
+    def do_serial_comms(self, command):
+        self.serial_port.write((command + '\n').encode("ascii"))
+        line = self.serial_port.readline().strip().decode('ascii')
+        return line
         
 
 def main(args=None):
 
     rclpy.init(args=args)
-
 
     try:
         triggerscopeserver = TriggerscopeServerNode()

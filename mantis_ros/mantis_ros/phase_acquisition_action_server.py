951a232f066b7b4bd51217a0fd6b483f04f86a71
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-11-15 10:03:11 -0800

    some implementation on phase acquisition server

--- mantis_ros/mantis_ros/phase_acquisition_action_server.py
+++ mantis_ros/mantis_ros/phase_acquisition_action_server.py
@@ -1,7 +1,7 @@
 import time
 
-from mantis_ros.action import PhaseAcquisition
-
+from mantis_msgs.action import PhaseAcquisition
+from triggerscope_msgs.srv import SetAnalogSequence
 import rclpy
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
@@ -14,23 +14,29 @@ class PhaseAcquisitionActionServer(Node):
     def __init__(self):
         super().__init__('phase_acquisition_action_server')
 
-        self._action_server = ActionServer(
+        self.volts_per_nm = 0.1
+
+        self.action_server = ActionServer(
             self,
             PhaseAcquisition,
-            'phase_acquisition_action_server',
+            'run_phase_acquisition',
             execute_callback=self.execute_callback,
             callback_group=ReentrantCallbackGroup(),
             goal_callback=self.goal_callback,
             cancel_callback=self.cancel_callback)
 
+        self.stage_client = self.create_client(SetAnalogSequence, 'set_analog_sequence')
+        self.get_logger().info('Phase Acquisition Action Server has been started')
+        
     def destroy(self):
-        self.phase_acquisition_action_server.destroy()
+        self.action_server.destroy()
         super().destroy_node()
 
     def goal_callback(self, goal_request):
         """Accept or reject a client request to begin an action."""
         # This server allows multiple goals in parallel
         self.get_logger().info('Received goal request')
+                
         return GoalResponse.ACCEPT
 
     def cancel_callback(self, goal_handle):
@@ -42,6 +48,19 @@ class PhaseAcquisitionActionServer(Node):
         """Execute a goal."""
         self.get_logger().info('Executing goal...')
 
+
+        self.stage_client.wait_for_service(timeout_sec=1.0)
+        if not self.stage_client.service_is_ready():
+            self.get_logger().error('Service not available')
+            goal_handle.abort()
+            return PhaseAcquisition.Result()
+        
+        spacing = goal_handle.request.spacing_nm
+
+        # Set the analog sequence
+        request = SetAnalogSequence.Request()        
+        #request.sequence = [i * self.volts_per_nm for i in range(0, 1000, spacing)]
+        
         # Append the seeds for the PhaseAcquisition sequence
         feedback_msg = PhaseAcquisition.Feedback()
 
@@ -50,9 +69,6 @@ class PhaseAcquisitionActionServer(Node):
 
         # Populate result message
         result = PhaseAcquisition.Result()
-        result.sequence = feedback_msg.sequence
-
-        self.get_logger().info('Returning result: {0}'.format(result.sequence))
 
         return result
 

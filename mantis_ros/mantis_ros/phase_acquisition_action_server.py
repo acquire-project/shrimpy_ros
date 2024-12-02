659730b22f9bf3cd6abe46c6de44fd6387efce80
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-12-02 15:48:13 -0800

    lots of little fixes to phase acquisition server

--- mantis_ros/mantis_ros/phase_acquisition_action_server.py
+++ mantis_ros/mantis_ros/phase_acquisition_action_server.py
@@ -15,8 +15,12 @@ class PhaseAcquisitionActionServer(Node):
     def __init__(self):
         super().__init__('phase_acquisition_action_server')
 
-        self.volts_per_nm = 0.1
+        self.volts_per_um = 3.0
         self.use_analog_sequence = True
+        
+        self.stage_analog_channel = self.declare_parameter("stage_analog_channel", 3).get_parameter_value().integer_value
+        self.light_source_digital_channel = self.declare_parameter("light_source_digital_channel", 0).get_parameter_value().integer_value
+        
         self.action_server = ActionServer(
             self,
             PhaseAcquisition,
@@ -29,7 +33,7 @@ class PhaseAcquisitionActionServer(Node):
         self.stage_sequence_client = self.create_client(SetAnalogSequence, 'set_analog_sequence')
         self.stage_move_client = self.create_client(SetAnalogOut, 'set_analog_out')
         self.stage_sequence_control_client= self.create_client(ControlAnalogSequence, 'control_analog_sequence')
-        self.light_source_client= self.create_client(SetDigitalOut, 'set_digit_out')
+        self.light_source_client= self.create_client(SetDigitalOut, 'set_digital_out')
 
         self.get_logger().info('Phase Acquisition Action Server has been started')
         
@@ -58,18 +62,16 @@ class PhaseAcquisitionActionServer(Node):
         self.stage_sequence_client.wait_for_service(timeout_sec=1.0)
         self.stage_sequence_control_client.wait_for_service(timeout_sec=1.0)
         self.light_source_client.wait_for_service(timeout_sec=1.0)
-        if not (self.stage_move_client.service_is_ready() 
-                and self.stage_sequence_client.service_is_ready()
-                and self.stage_sequence_control_client.service_is_ready() 
-                and self.light_source_client.service_is_ready()):
+        if not (self.stage_move_client.service_is_ready()):
             self.get_logger().error('Triggerscope services not available')
             goal_handle.abort()
             return PhaseAcquisition.Result()
         
         # Turn on the light source
-        light_request = SetDigitalOut.Request(channel_bank=0, state=[1,0,0,0,0,0,0,0])
-        light_request = self.light_source_client.call(request)
-        if result.success:
+        light_request = SetDigitalOut.Request(channel_bank=False, state=[False,]*8)
+        light_request.state[self.light_source_digital_channel] = True
+        light_result = self.light_source_client.call(light_request)
+        if light_result.success:
             self.get_logger().info('Successfully turned on light source')
         else:
             self.get_logger().error('Failed to turn on light source')
@@ -80,10 +82,10 @@ class PhaseAcquisitionActionServer(Node):
             
             # Clear the analog sequence
             control_request = ControlAnalogSequence.Request()
-            control_request.channel = 3
+            control_request.channel = self.stage_analog_channel
             control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_CLEAR
-            result = self.stage_sequence_control_client.call(control_request)
-            if result.success:
+            control_result = self.stage_sequence_control_client.call(control_request)
+            if control_result.success:
                 self.get_logger().info('Successfully cleared analog sequence')
             else: 
                 self.get_logger().error('Failed to clear analog sequence')
@@ -93,7 +95,7 @@ class PhaseAcquisitionActionServer(Node):
             
             # Set the analog sequence
             request = SetAnalogSequence.Request()
-            request.channel = 3
+            request.channel = self.stage_analog_channel
             request.voltages = [float(i) for i in range(0, 11)] 
             
             result = self.stage_sequence_client.call(request)
@@ -107,9 +109,9 @@ class PhaseAcquisitionActionServer(Node):
             
             # Start the analog sequence
             control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_START
-            result = self.stage_sequence_control_client.call(control_request)
+            control_result = self.stage_sequence_control_client.call(control_request)
             
-            if result.success:
+            if control_result.success:
                 self.get_logger().info('Successfully started analog sequence')
             else:
                 self.get_logger().error('Failed to start analog sequence')
@@ -125,7 +127,7 @@ class PhaseAcquisitionActionServer(Node):
             feedback_msg = PhaseAcquisition.Feedback()
 
             for i in sequence:
-                request.channel = 3
+                request.channel = self.stage_analog_channel
                 request.voltage = i
                 #await result = self.stage_move_client.call_async(request)
                 result = self.stage_move_client.call(request)
@@ -141,9 +143,9 @@ class PhaseAcquisitionActionServer(Node):
 
 
         # Turn on the off source
-        light_request.state[0] = 0
-        light_request = self.light_source_client.call(request)
-        if result.success:
+        light_request.state[self.light_source_digital_channel] = False
+        light_result = self.light_source_client.call(light_request)
+        if light_result.success:
             self.get_logger().info('Successfully turned on light source')
         else:
             self.get_logger().error('Failed to turn on light source')

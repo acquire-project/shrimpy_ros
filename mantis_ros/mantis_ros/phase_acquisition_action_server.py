c2189a8b94b0c17a832dc1acacb12a072202c23e
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-12-09 11:33:17 -0800

    remove use_analog_sequence conditional (that was only there because analog sequencing wasn't working at one point)

--- mantis_ros/mantis_ros/phase_acquisition_action_server.py
+++ mantis_ros/mantis_ros/phase_acquisition_action_server.py
@@ -21,7 +21,6 @@ class PhaseAcquisitionActionServer(Node):
         super().__init__('phase_acquisition_action_server')
 
         self.volts_per_um = 30.0 ** -1
-        self.use_analog_sequence = True
         
         self.stage_analog_channel = self.declare_parameter("stage_analog_channel", 3).get_parameter_value().integer_value
         self.light_source_digital_channel = self.declare_parameter("light_source_digital_channel", 0).get_parameter_value().integer_value
@@ -98,82 +97,58 @@ class PhaseAcquisitionActionServer(Node):
             goal_handle.abort()
             return PhaseAcquisition.Result()
         
-        if self.use_analog_sequence:
-                        
-            # Clear the analog sequence
-            control_request = ControlAnalogSequence.Request()
-            control_request.channel = self.stage_analog_channel
-            control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_CLEAR
-            control_result = self.stage_sequence_control_client.call(control_request)
-            if control_result.success:
-                self.get_logger().info('Successfully cleared analog sequence')
-            else: 
-                self.get_logger().error('Failed to clear analog sequence')
-                goal_handle.abort()
-                return PhaseAcquisition.Result()
-            
-            
-            # Set the analog sequence
-            request = SetAnalogSequence.Request()
-            request.channel = self.stage_analog_channel
-            step_size = (goal_handle.request.end_pos_um-goal_handle.request.start_pos_um)/(goal_handle.request.num_frames-1)
-            request.voltages = [float(goal_handle.request.start_pos_um + i*step_size)*self.volts_per_um for i in range(0, goal_handle.request.num_frames)]
-                        
-            result = self.stage_sequence_client.call(request)
-            if result.success:
-                self.get_logger().info('Successfully set analog sequence')
-            else:
-                self.get_logger().error('Failed to set analog sequence')
-                goal_handle.abort()
-                return PhaseAcquisition.Result()
-            
-            
-            # Start the analog sequence
-            control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_START
-            control_result = self.stage_sequence_control_client.call(control_request)
-            
-            if control_result.success:
-                self.get_logger().info('Successfully started analog sequence')
-            else:
-                self.get_logger().error('Failed to start analog sequence')
-                goal_handle.abort()
-                return PhaseAcquisition.Result()
-            
-            self.camera_action.wait_for_server(timeout_sec=1.0)
-
-            goal_msg = AcquireMultiFrame.Goal()
-            goal_msg.num_frames = len(request.voltages)
-            
-            camera_response = self.camera_action.send_goal(goal_msg)
-            if camera_response.status == GoalStatus.STATUS_SUCCEEDED:
-                self.get_logger().info('Successfully ran camera sequence')
-            else:
-                self.get_logger().error('Failed to run camera sequence')
-                goal_handle.abort()
-                return PhaseAcquisition.Result()
-            
+                    
+        # Clear the analog sequence
+        control_request = ControlAnalogSequence.Request()
+        control_request.channel = self.stage_analog_channel
+        control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_CLEAR
+        control_result = self.stage_sequence_control_client.call(control_request)
+        if control_result.success:
+            self.get_logger().info('Successfully cleared analog sequence')
+        else: 
+            self.get_logger().error('Failed to clear analog sequence')
+            goal_handle.abort()
+            return PhaseAcquisition.Result()
+        
+        
+        # Set the analog sequence
+        request = SetAnalogSequence.Request()
+        request.channel = self.stage_analog_channel
+        step_size = (goal_handle.request.end_pos_um-goal_handle.request.start_pos_um)/(goal_handle.request.num_frames-1)
+        request.voltages = [float(goal_handle.request.start_pos_um + i*step_size)*self.volts_per_um for i in range(0, goal_handle.request.num_frames)]
+                    
+        result = self.stage_sequence_client.call(request)
+        if result.success:
+            self.get_logger().info('Successfully set analog sequence')
         else:
-            # Set the analog sequence
-            request = SetAnalogOut.Request()        
-            sequence = [float(i) for i in range(0, 11)]
-            
-            # Append the seeds for the PhaseAcquisition sequence
-            feedback_msg = PhaseAcquisition.Feedback()
+            self.get_logger().error('Failed to set analog sequence')
+            goal_handle.abort()
+            return PhaseAcquisition.Result()
+        
+        
+        # Start the analog sequence
+        control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_START
+        control_result = self.stage_sequence_control_client.call(control_request)
+        
+        if control_result.success:
+            self.get_logger().info('Successfully started analog sequence')
+        else:
+            self.get_logger().error('Failed to start analog sequence')
+            goal_handle.abort()
+            return PhaseAcquisition.Result()
+        
+        self.camera_action.wait_for_server(timeout_sec=1.0)
 
-            for i in sequence:
-                request.channel = self.stage_analog_channel
-                request.voltage = i
-                #await result = self.stage_move_client.call_async(request)
-                result = self.stage_move_client.call(request)
-                if result.success:
-                    self.get_logger().info('Successfully set analog output to %d' % i)
-                else:
-                    self.get_logger().error('Failed to set analog output to %d' % i)
-                    goal_handle.abort()
-                    return PhaseAcquisition.Result()
-                #feedback_msg.current_position_nm = i * self.volts_per_nm
-                #goal_handle.publish_feedback(feedback_msg)
-                time.sleep(0.2)
+        goal_msg = AcquireMultiFrame.Goal()
+        goal_msg.num_frames = len(request.voltages)
+        
+        camera_response = self.camera_action.send_goal(goal_msg)
+        if camera_response.status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info('Successfully ran camera sequence')
+        else:
+            self.get_logger().error('Failed to run camera sequence')
+            goal_handle.abort()
+            return PhaseAcquisition.Result()
 
 
         # Stop the analog sequence

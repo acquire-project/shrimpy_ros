cbdfc19efd84c97bc5eca9df1134ee6b00574437
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-12-09 12:15:04 -0800

    streamline action execution by breaking out triggerscope calls into their own function and using exceptions to handle errors.

--- mantis_ros/mantis_ros/phase_acquisition_action_server.py
+++ mantis_ros/mantis_ros/phase_acquisition_action_server.py
@@ -63,124 +63,84 @@ class PhaseAcquisitionActionServer(Node):
         """Execute a goal."""
         self.get_logger().info('Executing goal...')
 
-
-        self.stage_move_client.wait_for_service(timeout_sec=1.0)
-        self.stage_sequence_client.wait_for_service(timeout_sec=1.0)
-        self.stage_sequence_control_client.wait_for_service(timeout_sec=1.0)
-        self.light_source_client.wait_for_service(timeout_sec=1.0)
-        self.camera_action.wait_for_server(timeout_sec=1.0)
-        
-        if not (self.stage_move_client.service_is_ready()):
-            self.get_logger().error('Triggerscope services not available')
+        try:
+            self.stage_move_client.wait_for_service(timeout_sec=1.0)
+            self.stage_sequence_client.wait_for_service(timeout_sec=1.0)
+            self.stage_sequence_control_client.wait_for_service(timeout_sec=1.0)
+            self.light_source_client.wait_for_service(timeout_sec=1.0)
+            self.camera_action.wait_for_server(timeout_sec=1.0)
+            
+            if not (self.stage_move_client.service_is_ready()):
+                self.get_logger().error('Triggerscope services not available')
+                raise RuntimeError
+            
+            # Set the analog range
+            set_range_request = SetAnalogRange.Request(channel=self.stage_analog_channel, range=SetAnalogRange_Request.VOLTAGE_RANGE_0_TO_10)
+            self.call_triggerscope(self.analog_range_client, set_range_request, 'set analog range')
+            
+            # Turn on the light source
+            light_request = SetDigitalOut.Request(channel_bank=False, state=[False,]*8)
+            light_request.state[self.light_source_digital_channel] = True
+            self.call_triggerscope(self.light_source_client, light_request, 'turn on light source')
+                        
+            # Clear the analog sequence
+            control_request = ControlAnalogSequence.Request(channel=self.stage_analog_channel, command=ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_CLEAR)
+            self.call_triggerscope(self.stage_sequence_control_client, control_request, 'clear analog sequence')
+            
+            # Set the analog sequence
+            step_size = (goal_handle.request.end_pos_um-goal_handle.request.start_pos_um)/(goal_handle.request.num_frames-1)
+            voltages = [float(goal_handle.request.start_pos_um + i*step_size)*self.volts_per_um for i in range(0, goal_handle.request.num_frames)]
+            request = SetAnalogSequence.Request(channel=self.stage_analog_channel, voltages=voltages)
+            self.call_triggerscope(self.stage_sequence_client, request, 'set analog sequence')
+            
+            # Start the analog sequence
+            control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_START
+            self.call_triggerscope(self.stage_sequence_control_client, control_request, 'start analog sequence')
+            
+            
+            # Call the AcquireMultiFrame action on the camera
+            self.camera_action.wait_for_server(timeout_sec=1.0)
+            goal_msg = AcquireMultiFrame.Goal()
+            goal_msg.num_frames = len(request.voltages)
+            
+            camera_response = self.camera_action.send_goal(goal_msg)
+            if camera_response.status == GoalStatus.STATUS_SUCCEEDED:
+                self.get_logger().info('Successfully ran camera sequence')
+            else:
+                self.get_logger().error('Failed to run camera sequence')
+                raise RuntimeError
+            
+            # Turn on the off source
+            light_request.state[self.light_source_digital_channel] = False
+            self.call_triggerscope(self.light_source_client, light_request, 'turn off light source')
+            
+            # Stop the analog sequence
+            control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_STOP
+            self.call_triggerscope(self.stage_sequence_control_client, control_request, 'stop analog sequence')
+            
+        except RuntimeError:
+            self.get_logger().error('Aborting goal')
             goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        set_range_request = SetAnalogRange.Request()
-        set_range_request.channel = self.stage_analog_channel
-        set_range_request.range = SetAnalogRange_Request.VOLTAGE_RANGE_0_TO_10
-        set_range_result = self.analog_range_client.call(set_range_request)
-        if set_range_result.success:
-            self.get_logger().info('Successfully set analog range')
-        else: 
-            self.get_logger().error('Failed to set analog range')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        # Turn on the light source
-        light_request = SetDigitalOut.Request(channel_bank=False, state=[False,]*8)
-        light_request.state[self.light_source_digital_channel] = True
-        light_result = self.light_source_client.call(light_request)
-        if light_result.success:
-            self.get_logger().info('Successfully turned on light source')
         else:
-            self.get_logger().error('Failed to turn on light source')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-                    
-        # Clear the analog sequence
-        control_request = ControlAnalogSequence.Request()
-        control_request.channel = self.stage_analog_channel
-        control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_CLEAR
-        control_result = self.stage_sequence_control_client.call(control_request)
-        if control_result.success:
-            self.get_logger().info('Successfully cleared analog sequence')
-        else: 
-            self.get_logger().error('Failed to clear analog sequence')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        
-        # Set the analog sequence
-        request = SetAnalogSequence.Request()
-        request.channel = self.stage_analog_channel
-        step_size = (goal_handle.request.end_pos_um-goal_handle.request.start_pos_um)/(goal_handle.request.num_frames-1)
-        request.voltages = [float(goal_handle.request.start_pos_um + i*step_size)*self.volts_per_um for i in range(0, goal_handle.request.num_frames)]
-                    
-        result = self.stage_sequence_client.call(request)
-        if result.success:
-            self.get_logger().info('Successfully set analog sequence')
-        else:
-            self.get_logger().error('Failed to set analog sequence')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        
-        # Start the analog sequence
-        control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_START
-        control_result = self.stage_sequence_control_client.call(control_request)
-        
-        if control_result.success:
-            self.get_logger().info('Successfully started analog sequence')
-        else:
-            self.get_logger().error('Failed to start analog sequence')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        self.camera_action.wait_for_server(timeout_sec=1.0)
-
-        goal_msg = AcquireMultiFrame.Goal()
-        goal_msg.num_frames = len(request.voltages)
-        
-        camera_response = self.camera_action.send_goal(goal_msg)
-        if camera_response.status == GoalStatus.STATUS_SUCCEEDED:
-            self.get_logger().info('Successfully ran camera sequence')
-        else:
-            self.get_logger().error('Failed to run camera sequence')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-
-
-        # Stop the analog sequence
-        control_request.command = ControlAnalogSequence_Request.ANALOG_SEQUENCE_COMMAND_STOP
-        control_result = self.stage_sequence_control_client.call(control_request)
-        
-        if control_result.success:
-            self.get_logger().info('Successfully stopped analog sequence')
-        else:
-            self.get_logger().error('Failed to stop analog sequence')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        # Turn on the off source
-        light_request.state[self.light_source_digital_channel] = False
-        light_result = self.light_source_client.call(light_request)
-        if light_result.success:
-            self.get_logger().info('Successfully turned off light source')
-        else:
-            self.get_logger().error('Failed to turn off light source')
-            goal_handle.abort()
-            return PhaseAcquisition.Result()
-        
-        self.get_logger().info('Successfully completed goal')
-        goal_handle.succeed()
+            self.get_logger().info('Successfully completed goal')
+            goal_handle.succeed()
 
         # Populate result message
         result = PhaseAcquisition.Result()
 
         return result
     
-
+    def call_triggerscope(self, server, request, logstring):
+        """Call a service on the Triggerscope."""
+            
+        result = server.call(request)
+        if result.success:
+            self.get_logger().info('Succeeded: ' + logstring)
+        else:
+            self.get_logger().error('Failed: ' + logstring)
+            raise RuntimeError
+            
+        return result
 
 def main(args=None):
     rclpy.init(args=args)

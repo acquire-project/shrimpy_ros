f97e97812259d6dc72943703d0121827ec148d8f
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-12-18 14:56:45 -0800

    more implementation.  start converting to using torch.tensors.  Needs much work still.

--- waveorder_ros/waveorder_ros/phase_reconstruction_node.py
+++ waveorder_ros/waveorder_ros/phase_reconstruction_node.py
@@ -3,32 +3,18 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
-import numpy as np
-import waveorder
+#import numpy as np
+from torch import Tensor
+from waveorder.models import phase_thick_3d
 
 class PhaseReconstructionNode(Node):
 
     def __init__(self):
         super().__init__('phase_reconstruction_node')
-        self.sub = self.create_subscription(Image, 'image_raw', self.image_callback, 10)
-        self.image_queue = []
+        self.sub = self.create_subscription(Image, 'image_raw', self.image_callback, 11)
         
         self.get_logger().info("Generating phase calibration")
-        self.generate_phase_calibration()
-        self.get_logger().info("Phase reconstruction node started")
-        
-    def image_callback(self, msg: Image):
-        self.image_queue.append(msg)
-        self.get_logger().info(f"Received image {msg.header.seq}, I now have {len(self.image_queue)} images")
-
-        if len(self.image_queue) == self.simulation_arguments["zyx_shape"][0]:
-            self.get_logger().info("Performing phase reconstruction")
-            self.perform_phase_reconstruction()
-            self.image_queue = []
-
-    def generate_phase_calibration(self):
-        
-        # calculate the phase calibration
+                # calculate the phase calibration
         self.simulation_arguments = {
             "zyx_shape": (11, 2448, 2048), # update to match data shape, probably (11, 2448, 2048)
             "yx_pixel_size": 0.345, # in um, camera has 3.45 um pixels and we use 10x magnification
@@ -42,12 +28,32 @@ class PhaseReconstructionNode(Node):
             "numerical_aperture_illumination": 0.4, # this is a rough estimate
             "numerical_aperture_detection": 0.45, # the objective has 0.45 NA detection
         }
+        zyx=self.simulation_arguments["zyx_shape"]
+        self.image_z_stack = Tensor.new_empty(zyx)
+        self.generate_phase_calibration()
+        self.get_logger().info("Phase reconstruction node started")
         
+    def image_callback(self, msg: Image):
+
+        self.image_z_stack[self.image_count] = msg.data
+        self.get_logger().info(f"Received image, I now have {self.image_count} images")
+        self.get_logger().info(f"{type(msg.data)}")
+
+        if len(self.image_queue) == self.simulation_arguments["zyx_shape"][0]:
+            self.get_logger().info("Performing phase reconstruction")
+            self.perform_phase_reconstruction()
+            self.image_count = 0
+        else:
+            self.image_count += 1
+
+    def generate_phase_calibration(self):
+        
+
         # Calculate transfer function
         (
             self.real_potential_transfer_function,
             self.imag_potential_transfer_function,
-        ) = waveorder.phase_thick_3d.calculate_transfer_function(
+        ) = phase_thick_3d.calculate_transfer_function(
             **self.simulation_arguments, **self.transfer_function_arguments
         )
 
@@ -56,15 +62,16 @@ class PhaseReconstructionNode(Node):
     
     def perform_phase_reconstruction(self):
         # concatenate images as a 3d array
-        image_volume = np.array([np.array(image.data) for image in self.image_queue])
-        
         self.get_logger().info(f"Image volume generated with shape {self.real_potential_transfer_function.shape}")
         
-        zyx_recon = waveorder.phase_thick_3d.apply_inverse_transfer_function(
-            image_volume,
+        # Reconstruct
+        zyx_recon = phase_thick_3d.apply_inverse_transfer_function(
+            self.image_z_stack,
             self.real_potential_transfer_function,
             self.imag_potential_transfer_function,
             self.transfer_function_arguments["z_padding"],
+            self.simulation_arguments["z_pixel_size"],
+            self.transfer_function_arguments["wavelength_illumination"]
         )
         
         

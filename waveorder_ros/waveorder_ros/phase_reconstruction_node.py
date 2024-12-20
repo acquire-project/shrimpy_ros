1b8d8a1c16ff65109962249191223ac967fb3573
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-12-20 23:26:58 +0000

    converted copy/pasted hardcoded paramters to ros parameters

--- waveorder_ros/waveorder_ros/phase_reconstruction_node.py
+++ waveorder_ros/waveorder_ros/phase_reconstruction_node.py
@@ -13,30 +13,33 @@ class PhaseReconstructionNode(Node):
     def __init__(self):
         super().__init__('phase_reconstruction_node')
         
+        
+        self.setup_parameters()
+        
         self.get_logger().info("Generating phase calibration")
         # calculate the phase calibration
-        self.simulation_arguments = {
-            "zyx_shape": (11, 2448, 2048), # update to match data shape, probably (11, 2448, 2048)
-            "yx_pixel_size": 0.345, # in um, camera has 3.45 um pixels and we use 10x magnification
-            "z_pixel_size": 3, # in um, 1V piezo steps correspond to 3 um steps, update as needed
-            "index_of_refraction_media": 1, # this is an air immersion objective
-        }
-
-        self.transfer_function_arguments = {
-            "z_padding": 5, # it help to add padding to avoid wrapping artifacts at the edges
-            "wavelength_illumination": 0.532, # in um, green light
-            "numerical_aperture_illumination": 0.4, # this is a rough estimate
-            "numerical_aperture_detection": 0.45, # the objective has 0.45 NA detection
-        }
         
-        zyx=self.simulation_arguments["zyx_shape"]
-        self.image_z_stack = torch.empty(zyx)
+        self.image_z_stack = torch.empty(self.get_parameter("zyx_shape").value, dtype=torch.uint8)
         self.generate_phase_calibration()
 
         self.image_count = 0
         self.sub = self.create_subscription(Image, 'image_raw', self.image_callback, 11)
         self.get_logger().info("Phase reconstruction node started")
 
+    def setup_parameters(self):
+        # Define the simulation and transfer function arguments as parameters
+        # These should be updated to match the microscope and camera setup
+        self.declare_parameter("zyx_shape", (11, 2448, 2048))
+        self.declare_parameter("yx_pixel_size", 0.345)
+        self.declare_parameter("z_pixel_size", 3)
+        self.declare_parameter("index_of_refraction_media", 1)
+        
+        self.declare_parameter("z_padding", 5)
+        self.declare_parameter("wavelength_illumination", 0.532)
+        self.declare_parameter("numerical_aperture_illumination", 0.4)
+        self.declare_parameter("numerical_aperture_detection", 0.45)
+    
+    
     def image_callback(self, msg: Image):
 
         # Copy image data into tensor
@@ -47,8 +50,7 @@ class PhaseReconstructionNode(Node):
         self.get_logger().info(f"Received image, I now have {self.image_count} images")
         self.get_logger().info(f"{type(msg.data)}")
 
-        if self.image_count == self.simulation_arguments["zyx_shape"][0]-1:
-            self.get_logger().info("Performing phase reconstruction")
+        if self.image_count == self.get_parameter("zyx_shape").value[0]-1:
             self.perform_phase_reconstruction()
             self.image_count = 0
         else:
@@ -61,22 +63,30 @@ class PhaseReconstructionNode(Node):
             self.real_potential_transfer_function,
             self.imag_potential_transfer_function,
         ) = phase_thick_3d.calculate_transfer_function(
-            **self.simulation_arguments, **self.transfer_function_arguments
+            zyx_shape=self.get_parameter("zyx_shape").value,
+            yx_pixel_size=self.get_parameter("yx_pixel_size").value,
+            z_pixel_size=self.get_parameter("z_pixel_size").value,
+            wavelength_illumination=self.get_parameter("wavelength_illumination").value,
+            z_padding=self.get_parameter("z_padding").value,
+            index_of_refraction_media=self.get_parameter("index_of_refraction_media").value,
+            numerical_aperture_illumination=self.get_parameter("numerical_aperture_illumination").value,
+            numerical_aperture_detection=self.get_parameter("numerical_aperture_detection").value,
         )
 
         self.get_logger().info(f"Phase calibration generated with shape {self.real_potential_transfer_function.shape}")
 
     
     def perform_phase_reconstruction(self):
-                
+        self.get_logger().info("Performing phase reconstruction")
+
         # Reconstruct
         zyx_recon = phase_thick_3d.apply_inverse_transfer_function(
             self.image_z_stack,
             self.real_potential_transfer_function,
             self.imag_potential_transfer_function,
-            self.transfer_function_arguments["z_padding"],
-            self.simulation_arguments["z_pixel_size"],
-            self.transfer_function_arguments["wavelength_illumination"]
+            self.get_parameter("z_padding").value,
+            self.get_parameter("z_pixel_size").value,
+            self.get_parameter("wavelength_illumination").value
         )
         
         self.get_logger().info(f"Reconstructed image w/ with shape {zyx_recon.shape}")

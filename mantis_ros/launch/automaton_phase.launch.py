512e18360d7676d8a4d81d938d141d641afa6eb3
Justin Eskesen <jeskesen@chanzuckerberg.com>
2025-01-29 10:41:43 -0800

    Refactor node configuration (#2)
    * added phase reconstruction to launch
    
    * added config file for automaton phase reconstruction (started with only the zarr writer).  Added phase zarr writer to launch
    
    * added raw zarr writer to lauch & config

--- mantis_ros/launch/automaton_phase.launch.py
+++ mantis_ros/launch/automaton_phase.launch.py
@@ -76,26 +76,32 @@ example_parameters = {
 
 def launch_setup(context, *args, **kwargs):
     """Launch camera driver node."""
-    parameter_file = LaunchConfig('parameter_file').perform(context)
+    camera_parameter_file = LaunchConfig('camera_parameter_file').perform(context)
     camera_type = LaunchConfig('camera_type').perform(context)
-    if not parameter_file:
-        parameter_file = PathJoinSubstitution(
+    if not camera_parameter_file:
+        camera_parameter_file = PathJoinSubstitution(
             [FindPackageShare('mantis_ros'), 'config', camera_type + '.yaml']
         )
     if camera_type not in example_parameters:
         raise Exception('no example parameters available for type ' + camera_type)
 
+    parameter_file = LaunchConfig('parameter_file').perform(context)
+    if not parameter_file:
+        parameter_file = PathJoinSubstitution(
+            [FindPackageShare('mantis_ros'), 'config', camera_type + '.yaml']
+        ) 
     
     camera_node = Node(
         package='spinnaker_camera_driver',
         executable='camera_driver_node',
         output='screen',
         name=[LaunchConfig('camera_name')],
+        #prefix=['gdbserver localhost:3000'],
         parameters=[
             example_parameters[camera_type],
             {
                 'ffmpeg_image_transport.encoding': 'hevc_nvenc',
-                'parameter_file': parameter_file,
+                'parameter_file': camera_parameter_file,
                 'serial_number': [LaunchConfig('serial')],
             },
         ],
@@ -118,6 +124,38 @@ def launch_setup(context, *args, **kwargs):
         name='phase_acquisition'
     )
     
+    phase_reconstruction = Node(
+        package='waveorder_ros',
+        executable='phase_reconstruction_node',
+        output='screen',
+        name='phase_reconstruction',
+        remappings=[
+            ('image_raw', '/flir_camera/image_raw'),
+        ],
+    )
+    
+    phase_zarr_writer = Node(
+        package='acquire_zarr',
+        executable='float_volume_zarr_writer',
+        output='screen',
+        name='phase_zarr_writer',
+        parameters= [parameter_file],
+        remappings=[
+            ('image_data', '/phase_reconstruction'),
+        ],
+    )   
+    
+    raw_zarr_writer = Node(
+        package='acquire_zarr',
+        executable='image_zarr_writer_node',
+        output='screen',
+        name='raw_image_zarr_writer',
+        parameters= [parameter_file],
+        remappings=[
+            ('image_data', '/flir_camera/image_raw'),
+        ],
+    )   
+    
     foxglove_bridge = Node(
         package='foxglove_bridge',
         executable='foxglove_bridge',
@@ -129,7 +167,10 @@ def launch_setup(context, *args, **kwargs):
         camera_node,
         triggerscope_node,
         phase_acquisition_server,
-        foxglove_bridge,
+        phase_reconstruction,
+        phase_zarr_writer,
+        raw_zarr_writer,
+        #foxglove_bridge,
     ]
     return nodes
 
@@ -153,9 +194,15 @@ def generate_launch_description():
                 description='FLIR serial number of camera (in quotes!!)',
             ),
             LaunchArg(
-                'parameter_file',
+                'camera_parameter_file',
                 default_value='',
                 description='path to ros parameter definition file (override camera type)',
+            ),
+            LaunchArg(
+                'parameter_file',
+                default_value=PathJoinSubstitution(
+                    [FindPackageShare('mantis_ros'), 'config', 'automaton_config.yaml']),
+                description='path to configuration YAML file for the configuration of ROS2 nodes',
             ),
             OpaqueFunction(function=launch_setup),
         ]
